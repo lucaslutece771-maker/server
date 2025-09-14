40d898ebe2130bc2761c3bec198e1665b317b7e3
datsik <lucaslutece771@gmail.com>
2025-09-14 15:09:32 -0300

    MKV

--- script.js
+++ script.js
@@ -1,8 +1,16 @@
 const agentes = {
-  "mariposa": "fks172",
-  "sidorovich": "lop1212",
-  "takukunavara": "prc95",
-  "borboletinha": "ngx12",
+  "Batman": "fks172",
+  "Sidorovich": "lop1212",
+  "Penny": "prc95",
+  "Nick Valentine": "ngx12",
+  "Sherlock": "lock1212",
+  "L Lawliet": "kirakira",
+  "Sam Fisher": "NKVD1232",
+  "Jill Valentine": "ocus612",
+  "Ada Wong": "umb0909",
+  "Ada Wrong": "kps??",
+  "Jimin do BTS": "kpop",
+  "Decoder": "#vT9!pLx2@rQz7$Wm8^Kd3*Yb6&Jo1Xe",
 };
 
 function validarLogin(event) {
